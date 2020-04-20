553b0e7d188d212984a3b25a550ba39793cde300
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-20 22:13:26 +0200

    move run code to internal/cmd/cmd.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- cmd/smc-sock/main.go
+++ cmd/smc-sock/main.go
@@ -1,37 +1,9 @@
 package main
 
 import (
-	"flag"
-
 	"github.com/hwipl/smc-sock/internal/cmd"
 )
 
-// variable definitions
-var (
-	server  bool   = false
-	client  bool   = false
-	address string = "127.0.0.1"
-	port    int    = 50000
-)
-
 func main() {
-	// parse command line arguments
-	flag.BoolVar(&server, "s", server, "run server")
-	flag.BoolVar(&client, "c", client, "run client")
-	flag.StringVar(&address, "a", address, "server/client address")
-	flag.IntVar(&port, "p", port, "server/client port")
-
-	flag.Parse()
-
-	// run server
-	if server {
-		cmd.RunServer(address, port)
-		return
-	}
-
-	// run client
-	if client {
-		cmd.RunClient(address, port)
-		return
-	}
+	cmd.Run()
 }
---
+++ internal/cmd/cmd.go
@@ -0,0 +1,34 @@
+package cmd
+
+import "flag"
+
+// variable definitions
+var (
+	server  bool   = false
+	client  bool   = false
+	address string = "127.0.0.1"
+	port    int    = 50000
+)
+
+// Run is the main entry point of the program
+func Run() {
+	// parse command line arguments
+	flag.BoolVar(&server, "s", server, "run server")
+	flag.BoolVar(&client, "c", client, "run client")
+	flag.StringVar(&address, "a", address, "server/client address")
+	flag.IntVar(&port, "p", port, "server/client port")
+
+	flag.Parse()
+
+	// run server
+	if server {
+		RunServer(address, port)
+		return
+	}
+
+	// run client
+	if client {
+		RunClient(address, port)
+		return
+	}
+}
