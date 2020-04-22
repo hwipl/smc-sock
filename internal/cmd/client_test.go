a27472f799d459eff330f3fe581d415e027bc215
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-22 21:38:41 +0200

    add more client tests
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/client_test.go
+++ internal/cmd/client_test.go
@@ -8,18 +8,38 @@ import (
 )
 
 func TestRunClient(t *testing.T) {
+	var want, got string
+
 	// start a server
 	l := prepareServer("127.0.0.1", 0)
 	go runServerLoop(l)
 
-	// run client
+	// get address and port for client
 	addr := strings.Split(l.Addr().String(), ":")
 	port, err := strconv.Atoi(addr[1])
 	if err != nil {
 		log.Fatal(err)
 	}
-	want := "Hello, world!\n"
-	got := RunClient(addr[0], port)
+
+	// run test with default text message
+	want = *text + "\n"
+	got = RunClient(addr[0], port)
+	if got != want {
+		t.Errorf("got = %s; want %s", got, want)
+	}
+
+	// run test with custom text message
+	*text = "Hello again!!!"
+	want = *text + "\n"
+	got = RunClient(addr[0], port)
+	if got != want {
+		t.Errorf("got = %s; want %s", got, want)
+	}
+
+	// run test with empty text message
+	*text = ""
+	want = *text + "\n"
+	got = RunClient(addr[0], port)
 	if got != want {
 		t.Errorf("got = %s; want %s", got, want)
 	}
