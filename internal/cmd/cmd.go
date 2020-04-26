d1527a8d941b288315153d2cb5d71de9535d8961
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-26 11:38:13 +0200

    update command line argument description
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -4,10 +4,11 @@ import "flag"
 
 var (
 	// command line arguments
-	server  = flag.Bool("s", false, "run server")
-	client  = flag.Bool("c", false, "run client")
-	address = flag.String("a", "127.0.0.1:50000", "server/client address")
-	text    = flag.String("t", "Hello, world!", "text to transfer")
+	server  = flag.Bool("s", false, "run as server")
+	client  = flag.Bool("c", false, "run as client")
+	address = flag.String("a", "127.0.0.1:50000",
+		"listen on (server) or connect to (client) `address`")
+	text = flag.String("t", "Hello, world!", "transfer `text`")
 )
 
 // Run is the main entry point of the program
